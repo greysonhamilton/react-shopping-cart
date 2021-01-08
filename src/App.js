6e641fe58f0b447b71877d5bbfc83602dbf29357
Greyson <greyson.r.hamilton@gmail.com>
2021-01-07 19:20:16 -0500

    into the provider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import { ProductContext } from './components/contexts/ProductContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -12,21 +13,30 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		
+		setCart([
+			...cart, 
+			item
+		])
+
 	};
+
+	co
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{products, addItem}} >
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
