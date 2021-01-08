0d3a4a20b235a2a030d3379ea38fbcf24d28331b
Greyson <greyson.r.hamilton@gmail.com>
2021-01-07 19:47:07 -0500

    shopping for context

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ProductContext } from './components/contexts/ProductContext';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -21,21 +22,33 @@ function App() {
 
 	};
 
-	co
+	const removeItem = (item) => {
+
+		setCart(
+			cart.filter((item) => {
+				if(item.id !== itemId){
+					return item;
+				}
+
+			}));
+
+	}
 
 	return (
 		<div className="App">
-			<ProductContext.Provider value={{products, addItem}} >
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{products, addItem}}>
+				<CartContext.Provider value={{cart, removeItem}}>
+					<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products />
-			</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
